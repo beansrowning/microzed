4c9d5fcf61dbe67a8650023b77d10eccbeae4742
beansrowning <sean.browning94@gmail.com>
2017-06-21 19:19:38 -0500

    Test Run

---
+++ README.md
@@ -0,0 +1,4 @@
+# A Microbit Micropython compiler in the Zed Editor
+### By Sean Browning
+
+## Based on ntoll's [https://github.com/ntoll/python-native-editor](python-native-editor), using his Hexilify script.
---
+++ compile.js
@@ -0,0 +1,20 @@
+var session = require("zed/session");
+var fs = require("zed/fs");
+var hexify = require("./hexlifyscript.js");
+var fm = require("./firmware.hex");
+
+/**
+ * input: text from python script
+ */
+return function(data) {
+  var path = data.path;
+  var text = data.inputs.text;
+  var hexpath = path.replace(/\.hex$/, ".py");
+  var hex = hexify.hexlifyScript(text);
+  var firmware = fm.text();
+  var insertion = ":::::::::::::::::::::::::::::::::::::::::::";
+  var combined = firmware.replace(insertion, hex);
+  return fs.WriteFile(hexpath, combined).then(function() {
+    return session.flashMessage(path, "Compiled Successfully", 500);
+  });
+};
