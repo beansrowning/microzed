0ae38f0ba7dfff60647d4d418c752adc95b3974c
beansrowning <sean.browning94@gmail.com>
2017-06-21 21:21:06 -0500

    debug

--- compile.js
+++ compile.js
@@ -10,12 +10,12 @@ return function(data) {
   var path = data.path;
   var text = data.inputs.text;
   var hexpath = path.replace(/\.hex$/, ".py");
-  var hex = hexify.hexlifyScript(text);
+  /* var hex = hexify.hexlifyScript(text);
   /* Debug
   var insertion = ":::::::::::::::::::::::::::::::::::::::::::";
   var combined = fm.replace(insertion, hex);
   */
-  return fs.WriteFile(hexpath, hex).then(function() {
+  return fs.WriteFile(hexpath, text).then(function() {
     return session.flashMessage(path, "Compiled Successfully", 500);
   });
 };
